e7bde8ebfe6ccae5d66e71c47d1003715e1ae795
ZhengyuanWang <43016364+ZhengyuanWang@users.noreply.github.com>
2019-05-03 03:34:20 -0400

    Update 394. Decode String.java

--- 394. Decode String.java
+++ 394. Decode String.java
@@ -1,3 +1,5 @@
+_________________________________________________________My Solution_______________________________________________________________
+
 class Solution {
     public String decodeString(String s) {
         Stack<StringBuilder> sta = new Stack<>();
@@ -45,5 +47,32 @@ class Solution {
             }
         }
         return ans.toString();
+    }
+}
+_____________________________________________________________Better Solution_______________________________________________________________
+class Solution {
+    int i= 0;
+    public String decodeString(String s) {
+        if(s == null || s.length() == 0) return "";
+        
+        StringBuilder sb = new StringBuilder();
+        int counter = 0;
+        while(i < s.length()){
+            char c = s.charAt(i++);
+            if('0'<=c && c<= '9'){
+                counter = 10*counter + (c-'0');
+            }else if(c == '['){
+                String sub = decodeString(s); // global variable i changed in the recursive function. So it will be the next char after '['
+                for(int i=0;i<counter;i++){
+                    sb.append(sub);
+                }
+                counter = 0;
+            }else if(c == ']'){
+                return sb.toString();
+            }else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }
