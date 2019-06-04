ced2af31cf27d209eefe2f1b2a6dba9d6e8b36e4
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-06-04 13:25:11 -0400

    Update 176. Second Highest Salary.sql

--- 176. Second Highest Salary.sql
+++ 176. Second Highest Salary.sql
@@ -5,3 +5,4 @@ SELECT
        ORDER BY Salary DESC
         LIMIT 1 OFFSET 1),
     NULL) AS SecondHighestSalary
+--Ifnull: similar to getOrDefualt. Limit 1: output 1 line, offset 1: ignore first line
