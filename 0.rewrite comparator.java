18389064d10910aa50d10a38169da5f27d40e259
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-06-10 18:25:45 -0400

    Update 0.rewrite comparator.java

--- 0.rewrite comparator.java
+++ 0.rewrite comparator.java
@@ -6,3 +6,20 @@
       return cnt2 - cnt1;// descedent order.
     }
   };
+
+____________________________________________________Comparator for HashMap(only sort entry but hashMap)_______________________________________________________________
+        //Override comparator, identifer is Map.Entry<TYPE,TYPE>
+        Comparator<Map.Entry<String, Integer>> sortByInter = new Comparator<Map.Entry<String, Integer>>(){
+            @Override
+            public int compare(Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2){
+                
+                // same value, sort by key in alphabetic order
+                if(e2.getValue() == e1.getValue())return e1.getKey().compareTo(e2.getKey());
+                // sort by value in descending order
+                return e2.getValue() - e1.getValue();
+            } 
+        };
+        // Collection sort should use list to sort entry;
+        List<Map.Entry<String, Integer>> sorted = new ArrayList(recorder.entrySet());
+        // Sort entry
+        Collections.sort(sorted, sortByInter);
