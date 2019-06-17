20a48c9deb2abcb2410eef9f17bb219bda72446d
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-06-16 22:16:26 -0400

    Update 0.Tricks.java

--- 0.Tricks.java
+++ 0.Tricks.java
@@ -3,3 +3,7 @@ return a = 1; // it will return 1.
 Array.copyOfRange is slower than List.toArray(new int[List.size()][]
 
 int[] temp = connect.get(i).stream().mapToInt(Integer::intValue).toArray(); // transfer list to array int[]
+
+// first condition in for is only exceuted once, second will be terminate condition to for loop, third can be a operation statement.
+for (i = l; ++i < newIntervals.length; newIntervals[i] = intervals[r++]);
+                                         
