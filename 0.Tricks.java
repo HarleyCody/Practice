4134b227eecee33cd326c29ede4a93141b0f93ca
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-06-16 22:09:05 -0400

    Update 0.Tricks.java

--- 0.Tricks.java
+++ 0.Tricks.java
@@ -1,3 +1,5 @@
 return a = 1; // it will return 1.
 
 Array.copyOfRange is slower than List.toArray(new int[List.size()][]
+
+int[] temp = connect.get(i).stream().mapToInt(Integer::intValue).toArray(); // transfer list to array int[]
