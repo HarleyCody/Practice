21078ef5065a311e58c48d4362f290ce2498c1e0
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-06-17 14:43:31 -0400

    Create 202. Happy Number.java

---
+++ 202. Happy Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    HashSet<Integer> recorder = new HashSet(); 
+    public boolean isHappy(int n) {
+        // only digit 1 and 7 can be convert to happy num
+        if(n == 1 || n == 7) return true;
+        // single digital or negative num is not happy num
+        if(n <= 0 || n < 10) return false;
+        // if circle merge, can not be happy
+        if(recorder.contains(n)) return false;
+        
+        recorder.add(n);
+        int newNum = 0, digit;
+        while(n>0){
+            digit = n % 10;
+            n /= 10;
+            newNum += digit * digit;
+        }
+        return isHappy(newNum);
+    }
+}
