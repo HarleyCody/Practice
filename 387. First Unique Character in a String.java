e61c7f4204b4208399603030408c5c29cbf2dfa7
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-06-24 20:42:25 -0400

    Update 387. First Unique Character in a String.java

--- 387. First Unique Character in a String.java
+++ 387. First Unique Character in a String.java
@@ -1,3 +1,23 @@
+_______________________________________________________________Better Solution_____________________________________________________________
+class Solution {
+    public int firstUniqChar(String s) {
+        if(s == null || s.length() == 0){
+            return -1;
+        }
+        int res = Integer.MAX_VALUE;
+        for(char c = 'a'; c <= 'z'; c++){// travel through alphabetics and try to find char in String
+            if(s.indexOf(c) == -1){// char is not in String
+                continue;
+            }
+            
+            if(s.indexOf(c) == s.lastIndexOf(c)){// first index == last index -> appears once;
+                res = Math.min(res, s.indexOf(c));// Compare with index of last char appearing once, choose smaller one -> approching tofirst one 
+            }
+        }
+        return res == Integer.MAX_VALUE ? -1 : res;
+    }
+}
+_______________________________________________________________My Solution_________________________________________________________________
 class Solution {
     public int firstUniqChar(String s) {
         HashMap< Character, Integer> recorder = new LinkedHashMap();// record frequency of every char
