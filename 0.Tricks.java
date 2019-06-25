a8255e9f05011bab8da2d6e627c5db2d8b63da2e
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-06-25 03:19:43 -0400

    Update 0.Tricks.java

--- 0.Tricks.java
+++ 0.Tricks.java
@@ -21,7 +21,7 @@ ArrayList	LinkedList
   LinkedList class can act as a list and queue both because it implements List and Deque interfaces.
 4) ArrayList is better for ‘storing’ and accessing data.
   LinkedList is better for ‘manipulating’ data.
-_________________________________________________________________________________________________________________________________________
+________________________________________________java.util.Arrays.binarySearch___________________________________________________________
 Description
 The java.util.Arrays.binarySearch(Object[] a, int fromIndex, int toIndex, Object key) method searches a range of the specified array 
 for the specified object using the binary search algorithm.
@@ -44,3 +44,49 @@ Return Value
 This method returns index of the search key, if it is contained in the array, else it returns (-(insertion point) - 1). 
 The insertion point is the point at which the key would be inserted into the array; the index of the first element in the 
 range greater than the key, or toIndex if all elements in the range are less than the specified key.
+
+__________________________________________________________________Union Find________________________________________________________________
+C:
+int getfather(int v)
+{
+    if (father[v]==v)
+      return v;
+    else
+    {
+        father[v]=getfather(father[v]);//路径压缩
+        return father[v];
+     }
+}
+//compress by rank
+void Union(int x ,int y)
+
+{
+     fx = getfather(x);
+     fy = getfather(y);
+
+     if (rank[fx]>rank[fy])
+        father[fy] = fx;
+     else
+     {
+        father[fx] = fy;
+        if(rank[fx]==rank[fy])
+           ++rank[fy]; //重要的是祖先的rank，所以只用修改祖先的rank就可以了，子节点的rank不用管
+     }
+}
+eg: private String find(String str){
+        if(father.get(str) == null) return null;
+        if(father.get(str).equals(str)) return str; // root found
+       
+        weight.put(str, weight.get(str) * weight.get(father.get(str))); // a/b , b/c -> a/c = a/b * b/c original father * ogfather / newfather 
+        father.put(str, find(father.get(str)));// update new connections, a/c 
+        return father.get(str);
+    }
+    
+    private void union(String a, String b, double w){
+        String root_a = find(a);
+        String root_b = find(b);
+        if(!root_a.equals(root_b)){
+            father.put(root_b, root_a);
+            weight.put(root_b, w * weight.get(a) / weight.get(b));
+        }
+    }
