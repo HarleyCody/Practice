2e463d317aca8abe32142536f9b33a44e84e94ff
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-06-28 15:55:14 -0400

    Update 277. Find the Celebrity.java

--- 277. Find the Celebrity.java
+++ 277. Find the Celebrity.java
@@ -1,3 +1,30 @@
+_______________________________________________________Better Solution_________________________________________________________
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int celebrity = 0;
+        
+        //find out candidate does not know anyone behind him
+        for(int i = 1; i < n ; ++i) {
+            if(knows(celebrity, i)) {
+                celebrity = i;
+            }
+        }
+        
+        // check if candidate knows any one before him and everybody knows him.
+        for(int i = 0; i < n; ++i){
+            if(i != celebrity){
+                if((i < celebrity && knows(celebrity, i)) || !knows(i, celebrity)){
+                    return -1;
+                }
+            }
+        }
+        return celebrity;
+        
+    }
+}
+_______________________________________________________My Solution_____________________________________________________________
 /* The knows API is defined in the parent class Relation.
       boolean knows(int a, int b); */
 
