564069d579072ce10668e6068c6f216db6673a81
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-07-01 13:59:07 -0400

    Update 0.Tricks.java

--- 0.Tricks.java
+++ 0.Tricks.java
@@ -1,6 +1,7 @@
 return a = 1; // it will return 1.
 
 Array.copyOfRange is slower than List.toArray(new int[List.size()][]
+use for loop to transfer is fastest way.
 
 int[] temp = connect.get(i).stream().mapToInt(Integer::intValue).toArray(); // transfer list to array int[]
 
