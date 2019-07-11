b4edc111f3f89b138f913b21cda66a9bc087ff67
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-07-11 14:31:04 -0400

    Create 250. Count Univalue Subtrees.java

---
+++ 250. Count Univalue Subtrees.java
@@ -0,0 +1,78 @@
+_________________________________________________________Best Solution_________________________________________________________
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private int result;
+    public int countUnivalSubtrees(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        result = 0;
+        helper(root);
+        return result;
+    }
+    
+    private int helper(TreeNode node) {
+        if (node.left == null && node.right == null) {
+            result ++;
+            return node.val;
+        }
+        int left = node.val;
+        if (node.left != null) {
+            left = helper(node.left);
+        }
+        
+         
+        int right = node.val;
+        
+        if (node.right != null) {
+            right = helper(node.right);
+        }
+        
+        if (left == Integer.MIN_VALUE || right == Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        if (left == right && right == node.val) {
+            result ++;
+            return node.val;
+        }
+        return Integer.MIN_VALUE;
+    }
+}
+_________________________________________________________My Solution___________________________________________________________
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int ans = 0;
+    public int countUnivalSubtrees(TreeNode root) {
+        if(root == null) return 0;
+        // traverse every node try to find a valid substree
+        if(isUniTree(root, root.val)){
+            ++ans;
+        }
+        countUnivalSubtrees(root.left);
+        countUnivalSubtrees(root.right);
+        return ans;
+    }
+    // check is valid
+    private boolean isUniTree(TreeNode root, int val){
+        if(root == null) return true;
+        if(root.val != val) return false;
+        return isUniTree(root.left, val) && isUniTree(root.right, val);
+    }
+}
