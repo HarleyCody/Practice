7a11ba2259a57aff0f868d41e8e87a7e281a5177
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-07-11 21:14:58 -0400

    Update 721. Accounts Merge.java

--- 721. Accounts Merge.java
+++ 721. Accounts Merge.java
@@ -67,3 +67,56 @@ class Solution {
                 
     }
 }
+__________________________________________________________Rote version___________________________________________________________________
+class Solution {
+    int[] fathers;
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        fathers = new int[accounts.size()];
+        for(int i = 0; i < fathers.length; ++i){
+            fathers[i] = i;
+        }
+        int fat = 0;
+        Map<String, Integer> records = new HashMap();
+        for(List<String> acts : accounts){
+            for(int i = 1; i < acts.size(); ++i){
+                Integer val = records.putIfAbsent(acts.get(i), fat);
+                if(val != null){
+                    union(fat, val);
+                }
+            }
+            ++fat;
+        }
+        
+        List<String>[] merge = new ArrayList[accounts.size()];
+        for(String key : records.keySet()){
+            int idx = getFather(records.get(key));
+            if(merge[idx] == null){
+                merge[idx] = new ArrayList();
+            }
+            merge[idx].add(key);
+        }
+        
+        List<List<String>> ans = new ArrayList();
+        for(int i = 0; i < merge.length; ++i){
+            if(merge[i] == null) continue;
+            List<String> med = new ArrayList();
+            med.add(accounts.get(i).get(0));
+            Collections.sort(merge[i]);
+            med.addAll(merge[i]);
+            ans.add(med);
+        }
+        return ans;
+    }
+    private void union(int fat, int val){
+        fathers[getFather(fat)] = getFather(val);
+    }
+    private int getFather(int v){
+        if(v == fathers[v]) return v;
+        int p = fathers[v];
+        while(p != fathers[p]){
+            p = fathers[p];
+            fathers[v] = p;
+        }
+        return p;
+    }
+}
