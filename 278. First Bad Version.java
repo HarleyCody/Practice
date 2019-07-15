597514957bec39309930a5fbc4dc9cecbd5a12a8
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-07-15 13:39:34 -0400

    Update 278. First Bad Version.java

--- 278. First Bad Version.java
+++ 278. First Bad Version.java
@@ -8,7 +8,9 @@ public class Solution extends VersionControl {
         while(start < end){
             // append by (end - start)/2 to avoid stackoverflow; (start + end) / 2 will cause overflow
             int mid = start + (end - start) / 2;
+            // mid is true, not a bad version, start from mid + 1
             if(!isBadVersion(mid)) start = mid + 1;
+            // mid is bad version, search from previous, try to minimize ans;
             else end = mid;
         }
         return start;
