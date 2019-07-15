70e944cada7d3173cd00c669da9fea91d5255a62
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-07-15 13:37:58 -0400

    Create 278. First Bad Version.java

---
+++ 278. First Bad Version.java
@@ -0,0 +1,16 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+
+        int start = 1, end = n;
+        while(start < end){
+            // append by (end - start)/2 to avoid stackoverflow; (start + end) / 2 will cause overflow
+            int mid = start + (end - start) / 2;
+            if(!isBadVersion(mid)) start = mid + 1;
+            else end = mid;
+        }
+        return start;
+    }
+}
