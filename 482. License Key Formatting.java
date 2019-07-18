e2e20e4bce38a64d88f369e6090ff283c0b17e07
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-07-18 12:19:32 -0400

    Update 482. License Key Formatting.java

--- 482. License Key Formatting.java
+++ 482. License Key Formatting.java
@@ -1,5 +1,6 @@
 class Solution {
     public String licenseKeyFormatting(String S, int K) {
+        // recorder acutal symbols
         Deque<Character> recorder = new LinkedList<Character>();
         int len = 0;
         for(char c : S.toCharArray()){
@@ -7,13 +8,17 @@ class Solution {
                 recorder.offer(c);
                 ++len;
             }
+            // change to uppercase
             if('a' <= c && c <= 'z'){
                 recorder.offer((char)(c - 32));
                 ++len;
             }
         }
         StringBuilder ans = new StringBuilder();
+        // append ansStringBuilder
         while(!recorder.isEmpty()){
+            // len == recorder.size() add beginning of string, else it = K
+            // len % K == 0; even format, every groups has K symbols, else add (len % k) symbols as beginning
             int it = len == recorder.size() ? len % K == 0 ? K : len % K : K;
             while(it-- > 0){
                 ans.append(recorder.poll());
