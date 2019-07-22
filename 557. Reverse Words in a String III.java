1d51f4652d96850ccf523129dc85bd76bb41ab15
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-07-22 11:19:04 -0400

    Update 557. Reverse Words in a String III.java

--- 557. Reverse Words in a String III.java
+++ 557. Reverse Words in a String III.java
@@ -1,3 +1,26 @@
+________________________________________________________Best Solution__________________________________________________________
+class Solution {
+    // change string to char array, find a word and swap char in a word 
+    public String reverseWords(String s) {
+        char[] c = s.toCharArray();
+        int start = 0, end = 0;
+        while((end = s.indexOf(' ', start)) != -1){
+            reverse(c, start, end - 1);
+            // move to begin of next word, if only move to end, next end will be == start;
+            start = end + 1;
+        }
+        reverse(c, start, s.length() - 1);
+        return new String(c);
+    }
+    private void reverse(char[] c, int start, int end){
+        while(start < end){
+            char tem = c[start];
+            c[start++] = c[end];
+            c[end--] = tem;
+        }
+    }
+}
+__________________________________________________________My Solution__________________________________________________________
 class Solution {
     // Split String to string array( string builder cannot use split method)
     public String reverseWords(String s) {
