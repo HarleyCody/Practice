e0781cea5c103a1f65f76ecb60f1a4f224df6493
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-08-09 15:36:15 -0400

    Update 733. Flood Fill.java

--- 733. Flood Fill.java
+++ 733. Flood Fill.java
@@ -1,3 +1,24 @@
+__________________________________________________________Best Solution_________________________________________________________
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if(image.length == 0) return image;
+        // if start point is the flooded point, terminate;
+        // do not need to use visited[][] to prevent stackoverflow. 
+        if(image[sr][sc] == newColor) return image;
+        color(image, sr, sc, newColor, image[sr][sc]);
+        return image;
+    }
+    public void color(int[][] image, int sr, int sc, int newColor, int og){
+        if(sr < 0 || sr == image.length || sc < 0 || sc == image[0].length || image[sr][sc] != og) return;
+        image[sr][sc] = newColor;
+        color(image, sr - 1, sc, newColor, og);
+        color(image, sr + 1, sc, newColor, og);
+        color(image, sr, sc + 1, newColor, og);
+        color(image, sr, sc - 1, newColor, og);
+    }
+}
+
+___________________________________________________________My Solution_________________________________________________________
 class Solution {
     // BFS , recursion to fill
     int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
