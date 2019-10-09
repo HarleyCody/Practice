f16414fee5c711029ed6f83861949013ab328294
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-10-08 20:48:48 -0400

    Update 1130. Minimum Cost Tree From Leaf Values.java

--- 1130. Minimum Cost Tree From Leaf Values.java
+++ 1130. Minimum Cost Tree From Leaf Values.java
@@ -1,3 +1,66 @@
+______________________________________________________________Best Solution(recursive)______________________________________________________________
+private int m_iSum = 0;
+
+public int MctFromLeafValues(int[] arr) {
+    helper(arr, 0, arr.Length - 1);
+    return m_iSum;
+}
+
+private int helper(int[] arr, int piStart, int piEnd) {
+    if ( piEnd < piStart )
+        return 0;
+    if ( piStart == piEnd ) {
+        return arr[piStart];
+    } else {
+        int liMax = piStart;
+        // find index of Max
+        for ( int i = piStart + 1; i <= piEnd; ++i )
+            if ( arr[i] > arr[liMax] )
+                liMax = i;
+        
+        int liLeft = helper(arr, piStart, liMax - 1);
+        int liRight = helper(arr, liMax + 1, piEnd);
+        
+        //put the max value in hieghest level, so the value is minized 
+        m_iSum += arr[liMax] * ( liLeft + liRight );
+        return arr[liMax];
+    }
+}
+______________________________________________________________Best Solution(stack)______________________________________________________________
+class Solution {
+/*
+Given an array A, choose two neighbors in the array a and b,
+we can remove the smaller one min(a,b) and the cost is a * b.
+What is the minimum cost to remove the whole array until only one left?
+
+To remove a number a, it needs a cost a * b, where b >= a.
+So a has to be removed by a bigger number.
+We want minimize this cost, so we need to minimize b.
+*/
+    public int mctFromLeafValues(int[] arr) {
+        if (arr.length == 0)return 0;
+        if (arr.length == 1)return arr[1];
+        Stack<Integer> s = new Stack<>();
+        int re = 0;
+        s.push(Integer.MAX_VALUE);
+        for (int i : arr) {
+            if (i <= s.peek()) {
+                s.push(i);
+            } else {
+                while (i > s.peek()) {
+                    int temp = s.pop();
+                    re += temp * Math.min(s.peek(), i);
+                }
+                s.push(i);
+            }
+        }
+        while (s.size() > 2) {
+            re += s.pop() * s.peek();
+        } 
+        return re;
+    }
+}
+________________________________________________________________My Solution____________________________________________________________
 class Solution {
     // record best result in range "start,end" avoid repeated calculation
     // split to left and right, caculate minSum and MaxValue in left and right respectively.
