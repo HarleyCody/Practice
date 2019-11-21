00820a829867437d996c5d93c508703edd0bf718
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-11-21 04:19:03 -0500

    Update 138. Copy List with Random Pointer.java

--- 138. Copy List with Random Pointer.java
+++ 138. Copy List with Random Pointer.java
@@ -1,66 +1,40 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node next;
-    public Node random;
-
-    public Node() {}
-
-    public Node(int _val,Node _next,Node _random) {
-        val = _val;
-        next = _next;
-        random = _random;
+_______________________________________________________Best Solution___________________________________________________________
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        copyList(head);
+        Node traverse = head;
+        // copy random
+        while(traverse != null){
+            if(traverse.random != null){
+                traverse.next.random = traverse.random.next;
+            }
+            traverse = traverse.next.next;
+        }
+        // seperate copy and original
+        // original linkedlist need to be recovered
+        Node ans = head.next;
+        Node split = ans;
+        while(head.next.next != null){
+            head.next = head.next.next;
+            split.next = split.next.next;
+            head = head.next;
+            split = split.next;
+        }
+        head.next = null;
+        return ans;
     }
-};
-*/
-public class Solution {
-  public Node copyRandomList(Node head) {
-
-    if (head == null) {
-      return null;
+    private void copyList(Node head){
+        while(head != null){
+            Node insert = new Node();
+            insert.val = head.val;
+            insert.next = head.next;
+            head.next = insert;
+            head = head.next.next;
+        }
     }
-
-    // Creating a new weaved list of original and copied nodes.
-    Node ptr = head;
-    while (ptr != null) {
-
-      // Cloned node
-      Node newNode = new Node(ptr.val);
-
-      // Inserting the cloned node just next to the original node.
-      // If A->B->C is the original linked list,
-      // Linked list after weaving cloned nodes would be A->A'->B->B'->C->C'
-      newNode.next = ptr.next;
-      ptr.next = newNode;
-      ptr = newNode.next;
-    }
-
-    ptr = head;
-
-    // Now link the random pointers of the new nodes created.
-    // Iterate the newly created list and use the original nodes' random pointers,
-    // to assign references to random pointers for cloned nodes.
-    while (ptr != null) {
-      ptr.next.random = (ptr.random != null) ? ptr.random.next : null;
-      ptr = ptr.next.next;
-    }
-
-    // Unweave the linked list to get back the original linked list and the cloned list.
-    // i.e. A->A'->B->B'->C->C' would be broken to A->B->C and A'->B'->C'
-    Node ptr_old_list = head; // A->B->C
-    Node ptr_new_list = head.next; // A'->B'->C'
-    Node head_old = head.next;
-    while (ptr_old_list != null) {
-      ptr_old_list.next = ptr_old_list.next.next;
-      ptr_new_list.next = (ptr_new_list.next != null) ? ptr_new_list.next.next : null;
-      ptr_old_list = ptr_old_list.next;
-      ptr_new_list = ptr_new_list.next;
-    }
-    return head_old;
-  }
 }
-_______________________________________________________My Solutions____________________________________________________________
+_______________________________________________________My Solution____________________________________________________________
 /*
 // Definition for a Node.
 class Node {
