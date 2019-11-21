3e26abc8d8fe44b61317bcee4d2e398ee37d814b
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-11-21 03:16:34 -0500

    Update 138. Copy List with Random Pointer.java

--- 138. Copy List with Random Pointer.java
+++ 138. Copy List with Random Pointer.java
@@ -60,3 +60,49 @@ public class Solution {
     return head_old;
   }
 }
+_______________________________________________________My Solutions____________________________________________________________
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+    public Node random;
+
+    public Node() {}
+
+    public Node(int _val,Node _next,Node _random) {
+        val = _val;
+        next = _next;
+        random = _random;
+    }
+};
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        Node ans = new Node();
+        Node copy = ans;
+        HashMap<Node, Node> copyMap = new HashMap();
+        Node og = head;
+        // copy next
+        while(og != null){
+            copy.val = og.val;
+            if(og.next != null) copy.next = new Node();
+            if(og.random != null) copy.random = new Node();
+            copyMap.put(og, copy);
+            og = og.next;
+            copy = copy.next;
+        }
+        // copy random
+        og = head;
+        copy = ans;
+        while(og != null){
+            if(og.random != null){
+                copy.random = copyMap.get(og.random);
+            }
+            copy = copy.next;
+            og = og.next;
+        }
+        return ans;
+    }
+}
