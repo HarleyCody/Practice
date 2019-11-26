4889281894f6a5213bae04f67a63c492a323e999
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-11-26 01:24:53 -0500

    Create 1155. Number of Dice Rolls With Target Sum.java

---
+++ 1155. Number of Dice Rolls With Target Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    int mod = 1000000007;
+    public int numRollsToTarget(int d, int f, int target) {
+        // dp[i][j] ith dice with target j;
+        // dp[i][j] == sum(dp[i - 1][j - f] to dp[i - 1][j - 1]);
+        int[][] dp = new int[d + 1][target + 1];
+        for(int i = 1; i <= Math.min(f, target); i++){
+            dp[1][i] = 1;
+        }
+        long sum;
+        for(int i = 2; i <= d; i++){
+            for(int j = i; j <= Math.min(i * f, target); j++){
+                sum  = 0;
+                for(int k = Math.max(j - f, 1); k < j; k++){
+                    sum += dp[i - 1][k]; 
+                }
+                dp[i][j] = (int) (sum % mod);
+            }
+        }
+        return dp[d][target];
+    }
+}
