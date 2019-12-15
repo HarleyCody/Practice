a83ee284cad3bd3fa25c3c86be7b7cbae35c3e62
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-12-15 02:29:43 -0500

    Create 694. Number of Distinct Islands.java

---
+++ 694. Number of Distinct Islands.java
@@ -0,0 +1,30 @@
+________________________________________________________Best Solution__________________________________________________________
+class Solution {
+// reocrd dirctions change of path of island
+    int[] dirs = new int[]{0, 1, 0, -1, 0};
+    int[][] map;
+    HashSet<String> recorder = new HashSet();
+    public int numDistinctIslands(int[][] grid) {
+        map = grid;
+        for(int i = 0; i < grid.length; ++i){
+            for(int j = 0; j < grid[0].length; ++j){
+                if(map[i][j] == 1){
+                    StringBuilder path = sinkIsland(i, j, new StringBuilder(), 's');
+                    if(!recorder.add(path.toString()))continue;
+                }
+            }
+        }
+        return recorder.size();
+    }
+    private StringBuilder sinkIsland(int x, int y, StringBuilder path, char dir){
+        path.append(dir);
+        map[x][y] = 0;
+        for(int i = 0; i < 4; ++i){
+            int nx = x + dirs[i];
+            int ny = y + dirs[i + 1];
+            if(nx < 0 || ny < 0 || nx >= map.length || ny >= map[0].length || map[nx][ny] == 0) continue;
+            sinkIsland(nx, ny, path, (char)i);
+        }
+        return path.append('b');
+    }
+}
