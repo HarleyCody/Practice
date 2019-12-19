5ce9f7a0786052d017578cade2a0e4e812613f29
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-12-19 04:34:23 -0500

    Update 437. Path Sum III.java

--- 437. Path Sum III.java
+++ 437. Path Sum III.java
@@ -1,3 +1,28 @@
+___________________________________________________Best Solution_______________________________________________________________
+class Solution {    
+     public int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap();
+        preSum.put(0,1);
+        return helper(root, 0, sum, preSum);
+    }
+    
+    public int helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) {
+            return 0;
+        }
+        
+        currSum += root.val;
+        // curSum - target: path is found, num of preSum that had occur in current path covers node val = 0;
+        int res = preSum.getOrDefault(currSum - target, 0);
+        preSum.put(currSum, preSum.getOrDefault(currSum, 0) + 1);
+        
+        res += helper(root.left, currSum, target, preSum) + helper(root.right, currSum, target, preSum);
+        // only record sum in current path; left or right path 
+        preSum.put(currSum, preSum.get(currSum) - 1);
+        return res;
+    }
+}
+___________________________________________________Solution(DFS)_______________________________________________________________
 class Solution {
     // pathSum = start from cur + start from left + start from right
     public int pathSum(TreeNode root, int sum) {
