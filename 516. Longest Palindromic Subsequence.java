3433f960eaf938a331ade9471417a3263f0a0648
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-12-19 22:50:13 -0500

    Update 516. Longest Palindromic Subsequence.java

--- 516. Longest Palindromic Subsequence.java
+++ 516. Longest Palindromic Subsequence.java
@@ -1,5 +1,31 @@
 __________________________________________________________Best Solution(2D)________________________________________________________________
-
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        // record maxlength for start at i end at j;
+        int[] dp = new int[n];
+        char[] ss = s.toCharArray();
+        Arrays.fill(dp,1);      // A single char is count as a palindrom with length 1
+        for (int j = 1; j < n; j++) {
+            // i and j start with different as 1, no character between them initialiy (curLong == 0)
+            // update dp for every level;
+            int curLong = 0;
+            for (int i = j - 1; i >= 0; --i) {
+                // retrive max palindorme length in last level.
+                int tmp = dp[i];    // dp[i][j]
+                if (ss[i] == ss[j]) {
+                    dp[i] = curLong + 2;
+                }
+                // record max palindrom length which is defintely within range i + 1 , j - 1;
+                // for next use. 
+                curLong = Math.max(curLong, tmp);
+            }
+        }
+        int max = 1;
+        for (int m : dp) max = Math.max(max, m);
+        return max;
+    }
+}
 ____________________________________________________________Dp Solution(2D)________________________________________________________________
 class Solution {
     // dp[i][j] longest palindrome length of string start at i end at j
