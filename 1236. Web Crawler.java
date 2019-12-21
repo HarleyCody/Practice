41e51d94282e203dc67c1ac32d2514058f03746e
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-12-21 05:00:02 -0500

    Update 1236. Web Crawler.java

--- 1236. Web Crawler.java
+++ 1236. Web Crawler.java
@@ -1,3 +1,38 @@
+________________________________________________________Best Solution(DFS)_______________________________________________________
+// use indexOf ("/", 7) instead of split("\\/)[2] to get host
+// use startwith(substring(0, idx) instead of host.equals(newHost)
+// faster
+class Solution {
+    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
+        String hostName = startUrl;
+        int ind = startUrl.indexOf("/", 7);
+        if( ind > 0) {
+            hostName = startUrl.substring(0, ind);
+        }
+        
+        Set<String> visited = new HashSet<>();
+        List<String> res = new ArrayList<>();
+        dfs(hostName, startUrl, htmlParser, visited, res);
+        
+        return res;
+    }
+    
+    
+    private void dfs(String hostName, String startUrl, HtmlParser htmlParser, Set<String> visited, List<String> res) {
+        if(visited.contains(startUrl)) 
+            return;
+        if(!startUrl.startsWith(hostName)) {
+            return;
+        }
+        visited.add(startUrl);
+        res.add(startUrl);
+        
+        List<String> urls = htmlParser.getUrls(startUrl);
+        for (String url: urls) {
+            dfs(hostName, url, htmlParser, visited, res);
+         }
+    }
+}
 _____________________________________________________My Solution(BFS)____________________________________________________________
 // get urls for every url in queue
 class Solution {
