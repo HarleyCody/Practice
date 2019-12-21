09f9ea9ed49634375411904e7b360fca6f60045b
Harley <43016364+HarleyCody@users.noreply.github.com>
2019-12-21 04:55:30 -0500

    Update 1236. Web Crawler.java

--- 1236. Web Crawler.java
+++ 1236. Web Crawler.java
@@ -23,3 +23,21 @@ class Solution {
         return s.split("\\/")[2];
     }
 }
+_____________________________________________________My Solution(DFS)____________________________________________________________
+class Solution {
+    HashSet<String> ans = new HashSet();
+    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
+        String host = getDomain(startUrl);
+        List<String> urls = htmlParser.getUrls(startUrl);
+        ans.add(startUrl);
+        for(String url : urls){
+            if(!ans.contains(url) && host.equals(getDomain(url))){
+                crawl(url, htmlParser);
+            }
+        }
+        return new ArrayList(ans);
+    }
+    private String getDomain(String s){
+        return s.split("\\/")[2];
+    }
+}
