79f94be40a72446baeea76f20c96cce38ff5c8c1
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-02-13 04:33:04 -0500

    Update 1004. Max Consecutive Ones III.java

--- 1004. Max Consecutive Ones III.java
+++ 1004. Max Consecutive Ones III.java
@@ -1,5 +1,22 @@
-________________________________________________________________My Solution________________________________________________________________
-// sliding window to control flippings
+________________________________________________________________Best Solution(Sliding window)______________________________________________
+class Solution {
+// left chase right only when left have to keep number of zero is K
+    public int longestOnes(int[] A, int k) {
+        int left = 0, right = 0, n = A.length;
+        while (right < n) {
+            // right keeps move to right
+            if (A[right++] == 0) k--;
+            if (k < 0) {
+                // left will chasing right with same pace, so distance keeps unchanged
+                if (A[left++] == 0) k++;
+            }
+        }
+        // distance between left and right will increase; as left only move when k < 0 but right always keep move to right
+        return right - left; // at the end, right == n
+    }
+}
+____________________________________________________________________My Solution(Sliding window)______________________________________________
+// Sliding window control number of flipping zero
 class Solution {
     public int longestOnes(int[] A, int K) {
         int l = 0, r = 0;
