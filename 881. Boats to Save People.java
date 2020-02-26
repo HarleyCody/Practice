e2514397881397b214ed4ced91fed3e9dd4cb401
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-02-25 23:33:53 -0500

    Create 881. Boats to Save People.java

---
+++ 881. Boats to Save People.java
@@ -0,0 +1,27 @@
+class Solution {
+    // two pointers, sort, choose left and choose right;
+    // if left + right > limit, only pick right;
+    public int numRescueBoats(int[] people, int limit) {
+        int len = people.length;
+        int numBoat = 0;
+        Arrays.sort(people);
+        int i = 0, j = len -1;
+        // set to valid right;
+        while(j >= 0 && people[j] >= limit){
+                ++numBoat;
+                --j;
+        }
+        // choose left and choose right
+        while(i <= j){       
+            if(people[i] + people[j] > limit){
+                ++numBoat;
+                --j;
+            }else{
+                ++i;
+                --j;
+                ++numBoat;
+            }
+        }
+        return numBoat;
+    }
+}
