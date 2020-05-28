143b9f5b2f1459b5ddf7e24080264762f89a237d
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-05-28 19:20:14 -0400

    Update 172. Factorial Trailing Zeroes.java

--- 172. Factorial Trailing Zeroes.java
+++ 172. Factorial Trailing Zeroes.java
@@ -1,5 +1,8 @@
+________________________________________________________________________Best Solution______________________________________________________
 class Solution {
-    //count how many 5 we have in n; 25 countains two 5s, so n == 25 ans = 25 / 5 + 5 / 5
+    // even number with 5 contribute a 0, number of 2 in N! will definitely larger than number of 5 in it. 
+    //So number of 0 relies on number of 5
+    //count how many 5 we have in n; 25 countains two 5s (25 = 5 * 5), so n == 25 ans = 25 / 5 + 5 / 5
     public int trailingZeroes(int n) {
         int ans = 0;
         while(n > 1){
