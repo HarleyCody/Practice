278d3aa02e3bb150cfadcf58e23e543a3bf754d4
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-07-03 11:22:26 -0400

    Create 951. Flip Equivalent Binary Trees.java

---
+++ 951. Flip Equivalent Binary Trees.java
@@ -0,0 +1,17 @@
+___________________________________________________________________________My Solution__________________________________________________________________________________________
+class Solution {
+    //dfs, current node can swap or not swap, just change the root in next recursion to "swap"
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null){
+            return true;
+        }else if(root1 == null || root2 == null){
+            return false;
+        }
+        if(root1.val != root2.val){
+            return false;
+        }
+        
+        return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) || 
+            (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+    }
+}
