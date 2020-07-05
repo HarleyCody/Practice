a184f2caf7a28f4d3029ee102826166aa67a42ed
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-07-05 01:02:02 -0400

    Update 1296. Divide Array in Sets of K Consecutive Numbers.java

--- 1296. Divide Array in Sets of K Consecutive Numbers.java
+++ 1296. Divide Array in Sets of K Consecutive Numbers.java
@@ -1,4 +1,50 @@
 class Solution {
+    // each group can be created as len = nums.length / k;
+    // recorder every num in correct group
+    public boolean isPossibleDivide(int[] nums, int k) {        
+        if (nums.length % k != 0) {
+            return false;
+        }
+        
+        int count = nums.length / k;
+        int[][] rows = new int[k][count];
+        int[] indexes = new int[k];
+        
+        for (int num : nums) {
+            int pos = num % k;
+            
+            if (indexes[pos] == count) {
+                return false;
+            } else {
+                rows[pos][indexes[pos]++] = num;
+            }
+        }
+        
+        for (int[] row: rows) {
+            Arrays.sort(row);
+        }
+        // gap assures consecutive as there can be a gap between group
+        // must be consecutive between one head to anther head(in this case head == 0)
+        for (int i = 0; i < count; ++i) {
+            boolean gap = false;
+            //test one by one;
+            for (int j = 1; j <= k; ++j) {
+                // j % k == 0 will be last valid num (ex, [4,5,6] 3,  6 is last valid num)
+                if (rows[j - 1][i] + 1 != rows[j % k][i]) {
+                    if (gap) {
+                        return false;
+                    } else {
+                        gap = true;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+}
+________________________________________________________________________________My Solution______________________________________________________________________________________
+class Solution {
     //only record num of consecutive nums and lastNum in consecutive array
     //update num one by one to array
     //also record range(ie, s and e) of num that can be updated
