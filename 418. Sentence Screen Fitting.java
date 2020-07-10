c623f13b962e8866f7acffb874660af8eafb3a88
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-07-09 20:39:31 -0400

    Create 418. Sentence Screen Fitting.java

---
+++ 418. Sentence Screen Fitting.java
@@ -0,0 +1,35 @@
+______________________________________________________________________________Best Solution______________________________________________________________________________
+// simulate one by one will TLE
+public class Solution {
+    // record end of starting from every word; to see how far a word can go;
+    // only calculate row rows, num of word of each line is dp[start] - start;
+    // update start to dp[start] / n; and calcualte next line;
+    public int wordsTyping(String[] sentence, int rows, int cols) {
+        // dp[i] records start at i, how far the sentence can go.
+        int[] dp = new int[sentence.length];
+        int n = sentence.length;
+        for(int i = 0, prev = 0, len = 0; i < n; ++i) {
+            // remove the length of previous word and space
+            if(len > 0){
+                len -= sentence[i - 1].length() + 1;
+            }
+            // calculate the start of next line.
+            // it's OK the index is beyond the length of array so that 
+            // we can use it to count how many words one row has.
+            while(len + sentence[prev % n].length() <= cols) len += sentence[prev++ % n].length() + 1;
+            dp[i] = prev;
+            System.out.println("end at word " + sentence[prev % n]);
+        }
+        int count = 0;
+        // add line by line
+        for(int i = 0, k = 0; i < rows; ++i) {
+            // count how many words one row has and move to start of next row.
+            // It's better to check if d[k] == k but I find there is no test case on it. 
+            // if(dp[k] == k) return 0;
+            count += dp[k] - k;
+            k = dp[k] % n;
+        }
+        // divide by the number of words in sentence
+        return count / n;
+    }
+}
