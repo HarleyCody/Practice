970364d034d8abbcdbad995374df8f028217f87a
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-07-16 18:17:04 -0400

    Update 382. Linked List Random Node.java

--- 382. Linked List Random Node.java
+++ 382. Linked List Random Node.java
@@ -1,3 +1,23 @@
+________________________________________________________________________________________Improved Solution__________________________________________________________________________
+class Solution {
+    // initialize random once and use forver
+    List<ListNode> recorder = new ArrayList();
+    Random rand = new Random();
+    int s;
+    public Solution(ListNode head) {
+        while(head != null){
+            recorder.add(head);
+            head = head.next;
+        }
+        s = recorder.size();
+    }
+    
+    /** Returns a random node's value. */
+    public int getRandom() {
+        int idx = rand.nextInt(s);
+        return recorder.get(idx).val;
+    }
+}
 _________________________________________________________________________________________My Solution__________________________________________________________________________
 class Solution {
     // Generate idx by random generator in Java
