019092478d62308c35a42c04b0cbb41d046a379b
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-07-18 16:51:51 -0400

    Update 241. Different Ways to Add Parentheses.java

--- 241. Different Ways to Add Parentheses.java
+++ 241. Different Ways to Add Parentheses.java
@@ -1,4 +1,35 @@
+_____________________________________________________________________________________Shorter but slower Solution___________________________________________________________________________
+public class Solution {
+    // only recursion might be slower than my solution as it does not mem
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> res = new ArrayList<Integer>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '-' || c == '+' || c == '*') {
+                String a = input.substring(0, i);
+                String b = input.substring(i + 1);
+                List<Integer> al = diffWaysToCompute(a);
+                List<Integer> bl = diffWaysToCompute(b);
+                for (int x : al) {
+                    for (int y : bl) {
+                        if (c == '-') {
+                            res.add(x - y);
+                        } else if (c == '+') {
+                            res.add(x + y);
+                        } else if (c == '*') {
+                            res.add(x * y);
+                        }
+                    }
+                }
+            }
+        }
+        if (res.size() == 0) res.add(Integer.valueOf(input));
+        return res;
+    }
+}
+______________________________________________________________________________________My Solution___________________________________________________________________________
 class Solution {
+    // recursion + mem
     //treat as tree
     //left rlt opr right rlt
     //return rlt for cur range
