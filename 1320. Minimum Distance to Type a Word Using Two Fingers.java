85a0a422ae93872b599f924d3216223ba9a05f64
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-07-21 17:33:31 -0400

    Update 1320. Minimum Distance to Type a Word Using Two Fingers.java

--- 1320. Minimum Distance to Type a Word Using Two Fingers.java
+++ 1320. Minimum Distance to Type a Word Using Two Fingers.java
@@ -1,3 +1,46 @@
+________________________________________________________________________________dp Solution_______________________________________________________________________________
+/*Imagine that we tap all letters with only one finger.
+The res distance we get is the maximum distance we will need.
+
+In our dynamic programming, dp[a] means that,
+if our left finger ends at character a,
+the maximum we can save is dp[a].
+
+Now our right finger tapped all letters, and left finger did nothing.
+We iterate through the whole string one by one
+and select some letter to tap with the left finger.
+By doing this, we want to find out the maximum distance that we can save from the tapping with one finger.
+
+Assume that our left finger is at a now,
+our right finger is at b,
+and we the right finger will tap c next.
+
+Instead of moving right finger from b to c with distance d(b, c),
+we try moving left finger from a to c with distance d(a, c).
+Hopely this will save d(b, c) - d(a, c).
+
+And finaly, we have one fingers at b and the other at c now.
+The finger at b will be new left finger, and the other will be the rihgt.*/
+
+class Solution {
+    public int minimumDistance(String word) {
+        int dp[] = new int[26], res = 0, save = 0, n = word.length();
+        for (int i = 0; i < n - 1; ++i) {
+            int b = word.charAt(i) - 'A', c = word.charAt(i + 1) - 'A';
+            for (int a = 0; a < 26; ++a)
+                dp[b] = Math.max(dp[b], dp[a] + d(b, c) - d(a, c));
+            save = Math.max(save, dp[b]);
+            res += d(b, c);
+        }
+        return res - save;
+
+    }
+
+    private int d(int a, int b) {
+        return Math.abs(a / 6 - b / 6) + Math.abs(a % 6 - b % 6);
+    }
+}
+    
 ________________________________________________________________________________My Best Solution________________________________________________________________________
 class Solution {
     // improve use array instead of hashMap
