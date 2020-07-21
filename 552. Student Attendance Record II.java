7eff6c8feb44a82e3e8f51f608b7b82e323c4e53
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-07-21 03:51:00 -0400

    Create 552. Student Attendance Record II.java

---
+++ 552. Student Attendance Record II.java
@@ -0,0 +1,58 @@
+___________________________________________________________________________________Best Solution O(logn) ____________________________________________________________________________
+https://leetcode.com/problems/student-attendance-record-ii/discuss/101633/Improving-the-runtime-from-O(n)-to-O(log-n)
+final int MOD = 1000000007;
+final int M = 6;
+
+int[][] mul(int[][] A, int[][] B) {
+    int[][] C = new int[M][M];
+    for (int i = 0; i < M; i++)
+        for (int j = 0; j < M; j++)
+            for (int k = 0; k < M; k++)
+                C[i][j] = (int) ((C[i][j] + (long) A[i][k] * B[k][j]) % MOD);
+    return C;
+}
+
+
+int[][] pow(int[][] A, int n) {
+    int[][] res = new int[M][M];
+    for (int i = 0; i < M; i++)
+        res[i][i] = 1;
+    while (n > 0) {
+        if (n % 2 == 1)
+            res = mul(res, A);
+        A = mul(A, A);
+        n /= 2;
+    }
+    return res;
+}
+
+public int checkRecord(int n) {
+    int[][] A = {
+            {0, 0, 1, 0, 0, 0},
+            {1, 0, 1, 0, 0, 0},
+            {0, 1, 1, 0, 0, 0},
+            {0, 0, 1, 0, 0, 1},
+            {0, 0, 1, 1, 0, 1},
+            {0, 0, 1, 0, 1, 1},
+    };
+    return pow(A, n + 1)[5][2];
+}
+___________________________________________________________________________________DP Solution O(n) ____________________________________________________________________________
+https://leetcode.com/problems/student-attendance-record-ii/discuss/101643/Share-my-O(n)-C%2B%2B-DP-solution-with-thinking-process-and-explanation
+public class Solution {
+    long M = 1000000007;
+    public int checkRecord(int n) {
+        long[] f = new long[n <= 5 ? 6 : n + 1];
+        f[0] = 1;
+        f[1] = 2;
+        f[2] = 4;
+        f[3] = 7;
+        for (int i = 4; i <= n; i++)
+            f[i] = ((2 * f[i - 1]) % M + (M - f[i - 4])) % M;
+        long sum = f[n];
+        for (int i = 1; i <= n; i++) {
+            sum += (f[i - 1] * f[n - i]) % M;
+        }
+        return (int)(sum % M);
+    }
+}
