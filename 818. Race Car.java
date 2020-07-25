57ea3f7086b97f2af285589b2cc589a6c23f1e87
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-07-25 02:57:43 -0400

    Create 818. Race Car.java

---
+++ 818. Race Car.java
@@ -0,0 +1,107 @@
+____________________________________________________________________________Best Solution_____________________________________________________________________________________________
+//dp
+class Solution {
+    // dp[i] record min step from 0 to reach i
+    int[] dp = new int[10001];
+    public int racecar(int t) {
+        if (dp[t] > 0) return dp[t];
+        int n = (int)(Math.log(t) / Math.log(2)) + 1;
+        if (1 << n == t + 1) {
+            dp[t] = n;
+        } else {
+            // ans for pass target go back + 1 for turning car
+            dp[t] = racecar((1 << n) - 1 - t) + n + 1;
+            for (int m = 0; m < n - 1; ++m) {
+                // turn go to n and turn to 2^m before reach so it will cost steps + n - 1 + m + 2;
+                // 2 for two turns, so 
+                dp[t] = Math.min(dp[t], racecar(t - (1 << (n - 1)) + (1 << m)) + n + m + 1);
+            }
+        }
+        return dp[t];
+    }
+}
+____________________________________________________________________________Best BFS_____________________________________________________________________________________________
+/*Prunning Idea: We should allow the car to be able to pass the target and then reverse the direction to go to the target. However, there should be an upper bound because there's no point to keep accelerating after passing the target.
+
+To find the upper bound, lets' consider an extreme case where the car is always accelerating, the number of acceleration is m, the car with position (2^m) - 1 has not reached the target: (2^m)-1 < target. Since the target is an integer, we can rewrite it as: target = (2^m)+k, k >= 0... (1)
+
+In the above extreme case, the car's speed 2^m is the maximum possible speed at the position (2^m)-1. Note that we should allow the car to be able to pass the target, which leads to the maximum next position is (2^m)-1 + 2^m... (2)
+
+Combine (1) and (2) we get (2^m)-1+2^m = 2*(2^m)-1 < 2*(2^m)+2*k = 2*target, k >= 0. It says, the upper bound for any position with any possible speed is 2*target.
+*/
+class Solution { 
+//prunning when doing bfs with idea above
+    public int racecar(int target) { 
+        Queue<int[]> q = new LinkedList();
+        HashMap<Integer, HashSet<Integer>> visited = new HashMap();
+        
+        q.offer(new int[]{0, 1});
+        
+        int steps = 0;
+        while(!q.isEmpty()){
+            
+            int size = q.size();
+            for(int s = 0; s < size; ++s){
+                int[] cur = q.poll();
+                int np = cur[0] + cur[1];
+                if(np == target || np == -target){
+                    return steps + 1;
+                }
+                int ns = cur[1] * 2;
+                if(0 < np && np < (target << 1) && (!visited.containsKey(np) || !visited.get(np).contains(ns))){
+                    q.offer(new int[]{np, ns});
+                    visited.putIfAbsent(np, new HashSet());
+                    visited.get(np).add(ns);
+                }
+                np = cur[0];
+                ns = cur[1] < 0 ? 1 : -1;
+                if(0 < np && np < (target << 1) && (!visited.containsKey(np) || !visited.get(np).contains(ns))){
+                    q.offer(new int[]{np, ns});
+                    visited.putIfAbsent(np, new HashSet());
+                    visited.get(np).add(ns);
+                } 
+            }
+            ++steps;
+        }
+        return -1;
+    }
+}
+____________________________________________________________________________My BFS(TLE)_____________________________________________________________________________________________
+// limitted prunning
+class Solution {
+    public int racecar(int target) { 
+        Queue<int[]> q = new LinkedList();
+        HashMap<Integer, HashSet<Integer>> visited = new HashMap();
+        
+        q.offer(new int[]{0, 1});
+        
+        int steps = 0;
+        while(!q.isEmpty()){
+            
+            int size = q.size();
+            for(int s = 0; s < size; ++s){
+                int[] cur = q.poll();
+                int np = cur[0] + cur[1];
+                if(np == target){
+                    return steps + 1;
+                }
+                int ns = cur[1] * 2;
+                if(!visited.containsKey(np) || !visited.get(np).contains(ns)){
+                    q.offer(new int[]{np, ns});
+                    visited.putIfAbsent(np, new HashSet());
+                    visited.get(np).add(ns);
+                }
+                np = cur[0];
+                ns = cur[1] < 0 ? 1 : -1;
+                if(!visited.containsKey(np) || !visited.get(np).contains(ns)){
+                    q.offer(new int[]{np, ns});
+                    visited.putIfAbsent(np, new HashSet());
+                    visited.get(np).add(ns);
+                } 
+            }
+            ++steps;
+        }
+        
+        return -1;
+    }
+}
