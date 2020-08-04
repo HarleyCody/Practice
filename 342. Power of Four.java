c6550a46b9bedae9486c9a7d8414b72519fb08cb
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-08-04 03:07:17 -0400

    Create 342. Power of Four.java

---
+++ 342. Power of Four.java
@@ -0,0 +1,21 @@
+class Solution {
+    // move bit by bit. move 1 bit == divide 2 so move twice in a loop
+    // times of 4 in the end will be inclusive to 1
+    public boolean isPowerOfFour(int num) {
+        int d = 0;
+        while(num > 1){
+            d = num & 1;
+            if(d == 1){
+                return false;
+            }
+            num >>= 1;
+            
+            d = num & 1;
+            if(d == 1){
+                return false;
+            }
+            num >>= 1;
+        }
+        return num == 1;
+    }
+}
