14a3daac9426ddbb73c169d9ddefb88a2d1c9b1d
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-08-07 07:02:49 -0400

    Create 466. Count The Repetitions.java

---
+++ 466. Count The Repetitions.java
@@ -0,0 +1,63 @@
+// my post https://leetcode.com/problems/count-the-repetitions/discuss/777733/Java-100Time-55.4-Space-only-Array-used.-In-detail-explaination.
+________________________________________________________________________________ My Solution_____________________________________________________________________________
+class Solution {
+    public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+        int len1 = s1.length(), len2 = s2.length();
+        boolean[] endAt = new boolean[len1];
+        char[] chs1 = s1.toCharArray(), chs2 = s2.toCharArray();
+        int[] nums1 = new int[len1], nums2 = new int[len1];
+        
+        int tltS1Len = len1 * n1;
+        
+        int startIdx = s1.indexOf(chs2[0]);
+        if(startIdx == -1){
+            return 0;
+        }
+        
+        int s2n = 0, s1n = 0, curIdx = 0, prevIdx = -1;
+        while(s1n * len1 + curIdx < tltS1Len){
+            for(char c : chs2){
+                curIdx = s1.indexOf(c, prevIdx + 1);
+                
+                if(curIdx == -1 && prevIdx == -1){
+                    return 0;
+                }
+                prevIdx = curIdx;
+                if(curIdx == -1){
+                    ++s1n;
+                    curIdx = s1.indexOf(c, prevIdx + 1);
+                }
+                prevIdx = curIdx;
+            }
+            
+            if(endAt[curIdx]){
+                break;
+            }
+            
+            prevIdx = curIdx;
+            ++s2n;
+            endAt[curIdx] = true;
+            nums1[curIdx] = s1n;
+            nums2[curIdx] = s2n; 
+        }
+        int cycleLen = (s1n - nums1[curIdx]) * len1;
+        if(cycleLen == 0){
+            return 0;
+        }
+        int nS2 = (tltS1Len - startIdx - 1) / cycleLen * s2n;
+        int remain = (tltS1Len - startIdx - 1) % cycleLen;
+        
+        int extraNum = 0;
+        for(int i = 0; i < len1; ++i){
+            if(!endAt[i]){
+                continue;
+            }
+            
+            if(remain >= i + nums1[i] * len1){
+                extraNum = Math.max(nums2[i], extraNum);
+            }
+        }
+        
+        return (nS2 + extraNum) / n2;
+    }
+}
