f1b039972255cb6bbd9f0a9d9ea2c5361b6c4c38
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-08-09 01:58:33 -0400

    Update 1429. First Unique Number.java

--- 1429. First Unique Number.java
+++ 1429. First Unique Number.java
@@ -1,3 +1,60 @@
+____________________________________________________________________________Best Solution_________________________________________________________________
+class FirstUnique {
+    // maintain doublelinkedlist to remove O(1)
+    // q only store valid unqiue num
+    private class Node {
+        int value;
+        Node prev, next;
+        
+        public Node(int value, Node prev, Node next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    private HashMap<Integer, Node> map;
+    private Node head = new Node(-1, null, null);
+    private Node tail = new Node(-1, head, null);
+    
+    public FirstUnique(int[] nums) {
+        head.next = tail;
+        map = new HashMap<>((int) Math.ceil(nums.length / 0.75f));
+        for (int n : nums) {
+            Node node = map.get(n);
+            if (node != null) {
+                removeVal(node);
+            } else addVal(n);
+        }
+    }
+    
+    public int showFirstUnique() {
+        if (head.next == null) return -1;        
+        else return head.next.value;
+    }
+    
+    public void add(int n) {
+        Node node = map.get(n);
+        if (node != null) {
+            removeVal(node);
+        } else addVal(n);
+    }
+    
+    private void removeVal(Node node) {
+        if (node.prev == null) return;
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        node.prev = null;
+        node.next = null;
+    }
+    
+    private void addVal(int val) {
+        Node node = new Node(val, tail.prev, tail);
+        tail.prev.next = node; // OVO SAM ZABORAVIO
+        tail.prev = node;
+        map.put(val, node);
+    }
+}
 ______________________________________________________________________________My Solution_________________________________________________________________
 // check freq before poll and return, only return if its unique
 class FirstUnique {
@@ -27,9 +84,3 @@ class FirstUnique {
     }
 }
 
-/**
- * Your FirstUnique object will be instantiated and called as such:
- * FirstUnique obj = new FirstUnique(nums);
- * int param_1 = obj.showFirstUnique();
- * obj.add(value);
- */
