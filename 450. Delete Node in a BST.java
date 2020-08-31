147134529ea98917f4af239f10a2a849ab58ccdc
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-08-31 10:51:24 -0400

    Update 450. Delete Node in a BST.java

--- 450. Delete Node in a BST.java
+++ 450. Delete Node in a BST.java
@@ -1,3 +1,64 @@
+______________________________________________________________________________Best Solution________________________________________________________________________________
+// delete node recursively
+// delete strategy: find target, set tar to min right or max left, and delete min right or max left from right or left; 
+class Solution {
+    
+    //Get the min value in the right subtree
+    public int getRightMin(TreeNode root){
+        root = root.right;
+        while(root.left != null)
+            root = root.left;
+        
+        return root.val;
+    }
+    // Get the max value in the left subtree
+    public int getLeftMax(TreeNode root){
+        root = root.left;
+        while(root.right != null)
+            root = root.right;
+        
+        return root.val;
+    }
+    // To delete the nodes
+    public TreeNode deleteNode(TreeNode root, int key) {
+        
+        // Base condition
+        if(root == null)
+           return root;
+        
+        // Iterate in the right of the tree
+        if(key > root.val)
+            root.right = deleteNode(root.right, key);
+        
+        // Iterate in the left of the tree
+        else if(key < root.val)
+            root.left = deleteNode(root.left, key);
+        
+        // Delete the current node
+        else{
+            
+            // If root is at the leave
+            if(root.left == null && root.right == null){
+                root = null;
+            }else if(root.right != null){
+                // Get the least value of the right substree
+                // Pass it as the delete key
+                // When your program raches it as it is leave so it will delete it
+                root.val = getRightMin(root);
+                root.right = deleteNode(root.right, root.val);
+            }else{
+                // Get the max value of the left subtree
+                // Pass it as the delete key
+                // When your program raches it as it is leave so it will delete it
+                root.val = getLeftMax(root);
+                root.left = deleteNode(root.left, root.val);
+            }
+            
+        }
+        
+        return root;
+    }
+}
 ________________________________________________________________________________My Solution________________________________________________________________________________
 // find target node with prev pointer and delete it by connecting prev and tar;
 // change tar according to tar.left and tar.right;
