d8a53d70d891132cb21147a9627ef9fba7f313fe
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-10-20 10:05:45 -0400

    Update 133. Clone Graph.java

--- 133. Clone Graph.java
+++ 133. Clone Graph.java
@@ -3,33 +3,44 @@
 class Node {
     public int val;
     public List<Node> neighbors;
-
-    public Node() {}
-
-    public Node(int _val,List<Node> _neighbors) {
+    
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    
+    public Node(int _val, ArrayList<Node> _neighbors) {
         val = _val;
         neighbors = _neighbors;
     }
-};
+}
 */
+// record node and value in map, connect with dfs, if next has been build retrieve from map otherwise continue connect and build new node
 class Solution {
-    // reocorder is a global variable, so it does not need to be a paramenter in recursion
     HashMap<Integer, Node> recorder = new HashMap();
     public Node cloneGraph(Node node) {
+        if(node == null){
+            return node;
+        }
         return clone(node);
     }
     
-    public Node clone(Node node){
-        // this node has been copied, returned copied node.
-        if(recorder.containsKey(node.val))return recorder.get(node.val);
-        // create a new node to copy.
-        Node newNode = new Node(node.val, new ArrayList<Node>());
-        // update recorder.
-        recorder.put(newNode.val, newNode);
-        // enlarge neighbor list, in the list, should be copied node -> recursion starts.
-        for(Node nodes : node.neighbors){
-            newNode.neighbors.add(clone(nodes)); 
+    private Node clone(Node node){
+        Node cur = new Node(node.val);
+        recorder.put(cur.val, cur);
+        for(Node n : node.neighbors){
+            if(recorder.containsKey(n.val)){
+                cur.neighbors.add(recorder.get(n.val));
+            }else{
+                cur.neighbors.add(clone(n));
+            }
         }
-        return newNode;
+        
+        return cur;
     }
 }
