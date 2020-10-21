21e4f5809be5a75b09ecb689335605c47291797b
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-10-21 11:47:13 -0400

    Update 735. Asteroid Collision.java

--- 735. Asteroid Collision.java
+++ 735. Asteroid Collision.java
@@ -1,3 +1,59 @@
+_________________________________________________________________________________My Solution(Best)______________________________________________________________________________
+// same as stack but using pointer to record size instead of sta
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        int idx = 0;
+        int len = asteroids.length;
+        for(int i = 0; i < len; ++i){
+            while(asteroids[i] < 0 && idx != 0 && asteroids[idx - 1] > 0 && 
+                  asteroids[idx - 1] < -asteroids[i]){
+                asteroids[--idx] = asteroids[i];
+            }
+            if(idx == 0 || asteroids[idx - 1] * asteroids[i] > 0 ||
+              asteroids[idx - 1] < 0 && asteroids[i] > 0){
+                asteroids[idx++] = asteroids[i];
+            }else if(asteroids[idx - 1] == -asteroids[i]){
+                --idx;
+            }
+        }
+        
+        int[] ans = new int[idx];
+        for(int i = 0; i < idx; ++i){
+            ans[i] = asteroids[i];
+        }
+        return ans;
+    }
+}
+
+____________________________________________________________________________My Concise Solution____________________________________________________________________________________
+// use stack to record asteroids that will not collide.
+// Collide happen when cur < 0 prev > 0; pop stack according to rules in question
+// if equal, do not push new asteroid to stack
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> sta = new Stack();
+        
+        for(int a : asteroids){
+            while(!sta.isEmpty() && sta.peek() > 0 && a < 0 && sta.peek() < -a){
+                sta.pop();
+            }
+            if(sta.isEmpty() || sta.peek() * a > 0 || sta.peek() < 0 && a > 0){
+                sta.push(a);
+            }else if(Math.abs(sta.peek()) == Math.abs(a)){
+                sta.pop();
+            }
+        }
+        int len = sta.size();
+        int[] ans = new int[len];
+        
+        for(int i = len - 1; 0 <= i; --i){
+            ans[i] = sta.pop();
+        }
+        
+        return ans;
+    }
+}
+____________________________________________________________________________My Solution Naive____________________________________________________________________________________
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         int len = asteroids.length;
