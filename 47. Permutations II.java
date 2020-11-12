ea77762d7bf027d4ff3f51f8f13a5ee2ed9e5d72
Harley <43016364+HarleyCody@users.noreply.github.com>
2020-11-12 18:16:53 -0500

    Update 47. Permutations II.java

--- 47. Permutations II.java
+++ 47. Permutations II.java
@@ -1,4 +1,56 @@
 ___________________________________________________________________________________Best Solution______________________________________________________________________
+// Core Idea; swap possibile pair. any swap can make permutation. all the swap combination conduct ans
+// Question alter to: swap cur or not; if(chosen number(duplicate) has been swapped to current before, donot swap anymore)
+// possible pair, chosen number is distinct among the number from cur and chosen number
+class Solution {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        
+        permute(nums, 0);
+        
+        return res;
+    }
+    
+    public void permute(int[] nums, int start) {
+        if (start == nums.length - 1) {
+            List<Integer> sub = new ArrayList<Integer>();
+            
+            for (int i : nums) {
+                sub.add(i);
+            }
+            res.add(sub);
+            
+            return;
+        }
+        
+        for (int i = start; i < nums.length; i++) {
+            if (i != start && !availableForPermute(nums, start, i)) continue; 
+            
+            swap(nums, start, i);
+            permute(nums, start + 1);
+            swap(nums, start, i);
+        }
+    }
+    // prunning, if any number between cur and chosen is same as chosen number, do not swap here
+    private boolean availableForPermute(int[] nums, int start, int chosen) {
+        for (int i = start; i < chosen; i++) {
+            if (nums[i] == nums[chosen]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private void swap(int[] nums, int s, int e) {
+        int temp = nums[s];
+        nums[s] = nums[e];
+        nums[e] = temp;
+    }
+}
 ____________________________________________________________________________________My Solution______________________________________________________________________
 class Solution {
     int len;
