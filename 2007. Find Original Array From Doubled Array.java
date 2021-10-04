ae16e39a8e57547bd8596c3a91e9c32c773170e4
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-10-04 15:54:41 -0400

    Update 2007. Find Original Array From Doubled Array.java

--- 2007. Find Original Array From Doubled Array.java
+++ 2007. Find Original Array From Doubled Array.java
@@ -1,3 +1,29 @@
+//Best Solution: Odd or even does not matter, only check if c can find 2 * c with greater frequency
+//Check 0 and len in the beginning to avoid computation
+class Solution {
+    public int[] findOriginalArray(int[] changed) {
+        int N = changed.length;
+        if(N % 2 == 1) return new int[0];
+        int max = 0;
+        for(int i : changed){
+            max = Math.max(max, i);
+        }
+        int[] arr = new int[max + 1];
+        for(int i : changed) arr[i] ++;
+        if(arr[0] % 2 == 1) return new int[0];
+        int[] res = new int[N / 2];
+        int idx = 0;
+        for(int num = 1; num <= max; num ++){
+            for(int ct = 0; ct < arr[num]; ct ++){
+                if(2 * num > max || arr[2 * num] == 0) return new int[0];
+                res[idx ++] = num;
+                arr[2 * num] --;
+                if(idx == N / 2) return res;
+            }
+        }
+        return res;
+    }
+}
 //My Solution: eliminate odd first then check even, only save c in ans, use while to save it
 class Solution {
     public int[] findOriginalArray(int[] changed) {
