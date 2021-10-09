b6e99846fac368cdc8192b6eca39ef992440f7f6
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-10-09 03:11:49 -0400

    Update 818. Race Car.java

--- 818. Race Car.java
+++ 818. Race Car.java
@@ -20,6 +20,28 @@ class Solution {
         return dp[t];
     }
 }
+// dp iterative way
+class Solution {
+    public int racecar(int target) {
+        int[] dp = new int[target + 3];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0; dp[1] = 1; dp[2] = 4;
+
+        for (int t = 3; t <= target; ++t) {
+            int k = 32 - Integer.numberOfLeadingZeros(t);
+            if (t == (1<<k) - 1) {
+                dp[t] = k;
+                continue;
+            }
+            for (int j = 0; j < k-1; ++j)
+                dp[t] = Math.min(dp[t], dp[t - (1<<(k-1)) + (1<<j)] + k-1 + j + 2);
+            if ((1<<k) - 1 - t < t)
+                dp[t] = Math.min(dp[t], dp[(1<<k) - 1 - t] + k + 1);
+        }
+
+        return dp[target];  
+    }
+}
 ____________________________________________________________________________Best BFS_____________________________________________________________________________________________
 /*Prunning Idea: We should allow the car to be able to pass the target and then reverse the direction to go to the target. However, there should be an upper bound because there's no point to keep accelerating after passing the target.
 
