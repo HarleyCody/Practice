2842a2c248227ed4658d0a613ace93ef61cb9dcc
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-10-11 09:43:16 -0400

    Update 146. LRU Cache.java

--- 146. LRU Cache.java
+++ 146. LRU Cache.java
@@ -1,3 +1,142 @@
+//My Solution: Behaviore break down: Set: Delete and add new node. Get delete and add old node
+public class LRUCache {
+    class Node{
+        int key;
+        int val;
+        Node prev;
+        Node next;
+        Node head;
+        Node tail;
+        public Node(int key, int val){
+            this.key = key;
+            this.val = val;
+        }
+    }
+    int cap;
+    int bucketSize;
+    Node[] bucket;
+    Node head;
+    Node tail;
+    public LRUCache(int capacity) {
+        cap = capacity;
+        bucketSize = 100;
+        bucket = new Node[bucketSize];
+    }
+
+    /*
+     * @param key: An integer
+     * @return: An integer
+     */
+    public int get(int key) {
+        // printLink(head);
+        // for(Node a : bucket){
+        //     printArr(a);
+        // }
+        Node find = findNode(key);
+        if(find != null){
+            removeNode(find);
+            set(find.key, find.val);
+        }
+        return find == null ? -1 : find.val;
+    }
+
+    /*
+     * @param key: An integer
+     * @param value: An integer
+     * @return: nothing
+     */
+    public void set(int key, int value) {
+        Node find = findNode(key);
+        if(find == null){
+            find = new Node(key, value);
+        }else{
+            removeNode(find);
+            find.val = value;
+        }
+        addNode(find);
+    }
+    private Node findNode(int key){
+        int idx = getIdx(key);
+        Node find = bucket[idx];
+        while(find != null){
+            if(find.key == key){
+                break;
+            }
+            find = find.next;
+        }
+        return find;
+    }
+    private void removeNode(Node node){
+        ++cap;
+        if(node.prev != null){
+            node.prev.next = node.next;
+        }else{
+            int idx = getIdx(node.key);
+            bucket[idx] = node.next;
+        }
+        if(node.next != null){
+            node.next.prev = node.prev;
+        }
+        if(node.tail != null){
+            node.tail.head = node.head;
+        }
+        if(node.head != null){
+            node.head.tail = node.tail;
+        }
+        if(node == head){
+            head = node.tail;
+        }
+        if(node == tail){
+            tail = node.head;
+        }
+        node.next = null;
+        node.prev = null;
+        node.head = null;
+        node.tail = null;
+    }
+    private void addNode(Node node){
+        if(head != null){
+            node.tail = head;
+            head.head = node;
+        }
+        head = node;
+        if(tail == null){
+            tail = node;
+        }
+        if(cap == 0){
+            tail = tail.head;
+            removeNode(tail.tail);
+        }
+        int idx = getIdx(node.key);
+        node.next = bucket[idx];
+        if(bucket[idx] != null){
+            bucket[idx].prev = node;
+        }
+        bucket[idx] = node;
+        --cap;
+    }
+    private int getIdx(int key){
+        return key % bucketSize;
+    }
+
+    private void printLink(Node head){
+        if(head == null) return;
+        Node n = head;
+        while(n != null){
+            System.out.print(n.key + " ");
+            n = n.tail;
+        }
+        System.out.println();
+    }
+    private void printArr(Node head){
+        Node n = head;
+        while(n != null){
+            System.out.print(n.key + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+}
 ________________________________________________________Implement LinkedHashMap__________________________________________________________
 // best and fastest solution
 // in the node its bi-direction
