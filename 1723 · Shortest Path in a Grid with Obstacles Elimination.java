2d495bab5254df2307c891bf2011e3b19909a12d
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-10-11 13:52:57 -0400

    Update 1723 · Shortest Path in a Grid with Obstacles Elimination.java

--- 1723 · Shortest Path in a Grid with Obstacles Elimination.java
+++ 1723 · Shortest Path in a Grid with Obstacles Elimination.java
@@ -1,3 +1,54 @@
+//Best Solution: DFS, finding stops when the path is Row + Col - 2 or limit is reached
+public class Solution {
+    public int n, m, mmin;
+    public int[] x_dir = new int[]{1, 0, -1, 0};
+    public int[] y_dir = new int[]{0, 1, 0, -1};
+
+    Boolean isValid(int x, int y){
+        if(x >= 0 && x < m && y >= 0 && y < n){
+            return true;
+        }
+        return false;
+    }
+    void dfs(int[][] grid,int x,int y,int k,int count){
+        if(!isValid(x, y) || grid[x][y]==-1){
+            return;
+        }
+
+        if((k == 0 && grid[x][y] == 1) || mmin == m + n - 2){
+            return;
+        }
+
+        if(x == m - 1 && y == n - 1){
+            if(count < mmin)
+                mmin = count;
+            return;
+        }
+        
+        if(grid[x][y] == 1)
+            k--;
+
+        int temp = grid[x][y];
+        grid[x][y] = -1;
+        for(int d = 0;d < 4;d++){
+            int tempx = x_dir[d] + x;
+            int tempy = y_dir[d] + y;
+            dfs(grid, tempx, tempy,k, count + 1);
+        }
+        grid[x][y] = temp;
+    }
+    public int shortestPath(int[][] grid, int k) {
+        // write your code here
+        int count=0;
+        mmin = 0x3f3f3f3f; 
+        m = grid.length;
+        n = grid[0].length;
+        
+        dfs(grid, 0, 0, k, count);
+
+        return mmin == 0x3f3f3f3f ? -1 : mmin;
+    }
+}
 // My Solution: BFS only append when arrive at grid[i][j] with different eliminate with in limit
 public class Solution {
     public int shortestPath(int[][] grid, int k) {
