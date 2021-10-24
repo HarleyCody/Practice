56738ae234cbe196742c208b4f10cc04d2dff56d
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-10-23 23:16:12 -0700

    Update 1166. Design File System.java

--- 1166. Design File System.java
+++ 1166. Design File System.java
@@ -1,3 +1,24 @@
+//Best Solution: As parent is rquired, every time only increase one level of path, so just need to check parent with hashMap() before put values;
+class FileSystem {
+
+    Map<String, Integer> file = new HashMap<>(); 
+    
+    public FileSystem() {
+        file.put("", -1);
+    }
+    
+    public boolean createPath(String path, int value) {
+        int idx = path.lastIndexOf("/");
+        String parent = path.substring(0, idx);
+        if (!file.containsKey(parent)) { return false; }
+        return file.putIfAbsent(path, value) == null;   
+    }
+    
+    public int get(String path) {
+        return file.getOrDefault(path, -1);
+    }
+}
+
 //My Solution: like trie, hashMap to record [kid string, kid node] pairs
 class FileSystem {
     class Node{
