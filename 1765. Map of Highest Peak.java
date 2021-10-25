67ba5f67a3c2039a7516da102fa525a5d1e7c628
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-10-25 08:51:59 -0700

    Update 1765. Map of Highest Peak.java

--- 1765. Map of Highest Peak.java
+++ 1765. Map of Highest Peak.java
@@ -1,3 +1,54 @@
+//Best Solution: BFS by array use array to be queue and two pinters start, end to memic offer and poll
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        int m = isWater.length;
+        int n = isWater[0].length;
+        int[] x = new int[n * m];
+        int[] y = new int[n * m];
+        int start = 0, end = -1;
+        int[][] ans = new int[m][n];
+        
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (isWater[i][j] == 1) {
+                    end++;
+                    ans[i][j] = 0;
+                    x[end] = i;
+                    y[end] = j;
+                } else
+                    ans[i][j] = -1;
+        while (start <= end) {
+            int i = x[start];
+            int j = y[start];
+            if (i > 0 && ans[i-1][j] == -1) {
+                ans[i-1][j] = ans[i][j] + 1;
+                end++;
+                x[end] = i-1;
+                y[end] = j;
+            }
+            if (i < m-1 && ans[i+1][j] == -1) {
+                ans[i+1][j] = ans[i][j] + 1;
+                end++;
+                x[end] = i+1;
+                y[end] = j;
+            }
+            if (j > 0 && ans[i][j-1] == -1) {
+                ans[i][j-1] = ans[i][j] + 1;
+                end++;
+                x[end] = i;
+                y[end] = j-1;
+            }
+            if (j < n-1 && ans[i][j+1] == -1) {
+                ans[i][j+1] = ans[i][j] + 1;
+                end++;
+                x[end] = i;
+                y[end] = j+1;
+            }
+            start++;
+        }
+        return ans;
+    }
+}
 //My Solution: BFS flood land to build height
 class Solution {
     public int[][] highestPeak(int[][] isWater) {
