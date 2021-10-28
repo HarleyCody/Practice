a7bf0929294a6ef1ee2423e8ff53a3a42b73e346
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-10-27 21:51:54 -0700

    Update 0.Data Structures.java

--- 0.Data Structures.java
+++ 0.Data Structures.java
@@ -154,3 +154,63 @@ class MyCalendar {
         return false;
     }
 }
+__________________________________________________________________________________Segment Tree______________________________________________________________________
+private static class SegmentTreeNode {
+    int l, r;
+    int k, lazy;
+    SegmentTreeNode left, right;
+        
+    SegmentTreeNode(int l, int r, int k) {
+        this.l = l;
+        this.r = r;
+        this.k = k;
+        this.lazy = 0;
+    }
+}
+
+private int query(SegmentTreeNode node, int i, int j) {    
+    if (i > j || node == null || i > node.r || j < node.l) return 0;
+    
+    if (i <= node.l && node.r <= j) return node.k;
+   
+	normalize(node);
+
+    return Math.max(query(node.left, i, j), query(node.right, i, j));
+}
+
+private void update(SegmentTreeNode node, int i, int j, int val) {
+    if (i > j || node == null || i > node.r || j < node.l) return;
+    
+    if (i <= node.l && node.r <= j) {
+		node.k += val;
+        node.lazy += val;
+        return;
+    }
+	
+    normalize(node);
+	
+    update(node.left, i, j, val);
+    update(node.right, i, j, val);
+    
+    node.k = Math.max(node.left.k, node.right.k);
+}
+//Make node balance by the middle, balance the tree
+//push the lazyness down to the child for balancing;
+private void normalize(SegmentTreeNode node) {
+    if (node.l < node.r) {
+        if (node.left == null || node.right == null) {
+            int m = node.l + (node.r - node.l) / 2;
+            node.left = new SegmentTreeNode(node.l, m, node.k);
+            node.right = new SegmentTreeNode(m + 1, node.r, node.k);
+        
+        } else if (node.lazy > 0) {
+            node.left.k += node.lazy;
+			node.left.lazy += node.lazy;
+			
+			node.right.k += node.lazy;
+            node.right.lazy += node.lazy;
+        }
+    }
+    
+    node.lazy = 0;
+}
