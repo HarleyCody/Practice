432edd38217023862caa79253b8f8499e6dbf00e
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-11-11 10:13:01 -0500

    Update 2034. Stock Price Fluctuation.java

--- 2034. Stock Price Fluctuation.java
+++ 2034. Stock Price Fluctuation.java
@@ -44,11 +44,74 @@ class StockPrice {
     }
 }
 
-/**
- * Your StockPrice object will be instantiated and called as such:
- * StockPrice obj = new StockPrice();
- * obj.update(timestamp,price);
- * int param_2 = obj.current();
- * int param_3 = obj.maximum();
- * int param_4 = obj.minimum();
- */
+//My Solution: Segment Tree record min max within a range
+class StockPrice {
+    class Node{
+        int left;
+        int right;
+        int min;
+        int max;
+        Node lKid;
+        Node rKid;
+        public Node(int h, int t, int v){
+            left = h;
+            right = t;
+            min = v;
+            max = v;
+        }
+        
+        public void update(int time, int price){
+            int mid = (left + right) >> 1;
+            if(left != right){
+                if(time <= mid){
+                    if(lKid == null){
+                        lKid = new Node(left, mid, price);
+                    }
+                    lKid.update(time, price);
+                }else{
+                    if(rKid == null){
+                        rKid = new Node(mid + 1, right, price);
+                    }
+                    rKid.update(time, price);
+                }
+            }
+            min = price;
+            max = price;
+            if(lKid != null){
+                min = Math.min(min, lKid.min);
+                max = Math.max(max, lKid.max);
+            }
+            if(rKid != null){
+                min = Math.min(min, rKid.min);
+                max = Math.max(max, rKid.max);
+            }
+        }
+    }
+    Node root;
+    int curTime;
+    int cur;
+    public StockPrice() {
+        root = new Node(0, (int)1e9, 0);
+    }
+    
+    public void update(int timestamp, int price) {
+        root.update(timestamp, price);
+        if(curTime <= timestamp){
+            curTime = timestamp;
+            cur = price;
+        }
+    }
+    
+    public int current() {
+        return cur;
+    }
+    
+    public int maximum() {
+        return root.max;
+    }
+    
+    public int minimum() {
+        return root.min;
+    }
+}
+
