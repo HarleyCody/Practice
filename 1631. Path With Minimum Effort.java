272b8b4031571a62d496fa6c58561bab1d4a7f3a
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-11-19 15:02:02 -0500

    Update 1631. Path With Minimum Effort.java

--- 1631. Path With Minimum Effort.java
+++ 1631. Path With Minimum Effort.java
@@ -1,3 +1,43 @@
+//Binary Search: Pick a thresh hold use dfs to defect if it can go to R - 1 and C - 1
+class Solution {
+    public int minimumEffortPath(int[][] heights) { 
+        int s=0;
+        int e = Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
+        while(s<=e){
+            int mid = s + (e - s) / 2;
+            boolean [][] visited= new boolean[heights.length][heights[0].length];
+            boolean isMin= dfs(0,0,heights[0].length,heights.length,mid,heights,visited);
+            if(isMin){
+                ans = Math.min(ans,mid);
+                 e = mid - 1;
+            }else {
+                 s = mid + 1;
+             }
+        }
+        return ans;
+    }
+    
+    
+   boolean dfs(int i, int j, int n, int m, int maxEffort, int[][] matrix, boolean[][] visited) {
+       if (i == m - 1 && j == n - 1) return true;
+       visited[i][j] = true;
+       boolean p = false;
+        if (i + 1 < m && !visited[i + 1][j] && Math.abs(matrix[i+1][j] - matrix[i][j])<=maxEffort) {
+            p = p || dfs(i + 1, j, n, m, maxEffort, matrix, visited);
+        }
+        if (j + 1 < n && !visited[i][j + 1] && Math.abs(matrix[i][j+1] - matrix[i][j])<=maxEffort) {
+            p = p ||dfs(i, j + 1, n, m,maxEffort, matrix, visited);
+        }
+        if (i - 1 >= 0 && !visited[i - 1][j] && Math.abs(matrix[i-1][j] - matrix[i][j])<=maxEffort) {
+            p = p ||dfs(i - 1, j, n, m, maxEffort, matrix, visited);
+        }
+        if (j - 1 >= 0 && !visited[i][j - 1] && Math.abs(matrix[i][j] - matrix[i][j-1])<=maxEffort) {
+            p = p ||dfs(i, j - 1, n, m, maxEffort, matrix, visited);
+        }
+       return p;
+    }
+}
 //My Solution: Djistra traverse all node and effort is the key to sort in pq
 class Solution {
     public int minimumEffortPath(int[][] heights) {
