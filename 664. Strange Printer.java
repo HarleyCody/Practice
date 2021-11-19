9db890d371a925273de0c6581d6d11924ef54251
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-11-18 23:23:39 -0500

    Update 664. Strange Printer.java

--- 664. Strange Printer.java
+++ 664. Strange Printer.java
@@ -1,3 +1,47 @@
+//Best Solution: Same dp idea, dp[l][r] = dp[l][k] + dp[k + 1][r - 1];
+//Skip the duplicate when finding k
+class Solution {
+    public int strangePrinter(String s) {
+        char[] chs = s.toCharArray();
+        int len = chs.length;
+        
+        if (len < 2) return len;
+        
+        int[] cur = new int[26];
+        int[] next = new int[len];
+        Arrays.fill(next, -1);
+        Arrays.fill(cur, -1);
+        cur[chs[0] - 'a'] = 0;
+        int size = 1;
+        
+        //set index from current index to next same char for sake of finding middle faster
+        for (int i = 1; i < len; ++i) {
+            if (chs[i - 1] == chs[i]) 
+                continue;
+            
+            int idx = chs[i] - 'a';
+            
+            if (cur[idx] != -1) 
+                next[cur[idx]] = size;
+            
+            cur[idx] = size++;
+        }
+        
+        int[][] dp = new int[size][size];
+        for (int l = size - 1; l >= 0; --l) {
+            Arrays.fill(dp[l], Integer.MAX_VALUE);
+            for (int m = l; m < size; ++m) {
+                if (m == l) 
+                    dp[l][m] = 1;
+                else 
+                    dp[l][m] = Math.min(dp[l][m], dp[l][m - 1] + 1);
+                for (int r = next[m]; r != -1; r = next[r]) 
+                    dp[l][r] = Math.min(dp[l][r], dp[l][m] + dp[m + 1][r - 1]);
+            }
+        }
+        return dp[0][size - 1];
+    }
+}
 //DP: Dp[left][right]: min print from left to right;
 //Dp[left][right] = dp[left][k] + dp[k + 1][right - 1]
 //This could be use to get the operation of string which can be divided as subquestions
