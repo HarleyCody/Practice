345d5eb821b8c61163d7456953bd4d3c6decdd82
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-12-11 14:09:23 -0500

    Create 926. Flip String to Monotone Increasing.java

---
+++ 926. Flip String to Monotone Increasing.java
@@ -0,0 +1,22 @@
+//Best Solution: Dynamic programming, curStatus depends one the previous
+// if cur == 1; curMinStepEndAtOne = Math.min(prevStepsEndAtZero, prevStepsEndAtOne); curMinStepEndAtZero = prevStepsEndAtZero + 1; 1 could be curChar - '0'
+// if cur == 0; curMinStepEndAtOne = Math.min(prevStepsEndAtZero, prevStepsEndAtOne) + 1; curMinStepEndAtZero = prevStepsEndAtZero; 1 cound be '1' - curChar;
+
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        char[] chs = s.toCharArray();
+        int len = chs.length;
+        int endAtOne = 0;
+        int endAtZero = 0;
+        for(char c : chs){
+            endAtOne = Math.min(endAtOne, endAtZero);
+            if(c == '1'){
+                ++endAtZero;
+            }else{
+                ++endAtOne;
+            }
+        }
+        
+        return Math.min(endAtOne, endAtZero);
+    }
+}
