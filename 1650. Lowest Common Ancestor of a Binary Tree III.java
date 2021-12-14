8bd6d31b88df45d2f0d0672d620e976da5b2aa76
Harley <43016364+HarleyCody@users.noreply.github.com>
2021-12-14 13:30:12 -0500

    Update 1650. Lowest Common Ancestor of a Binary Tree III.java

--- 1650. Lowest Common Ancestor of a Binary Tree III.java
+++ 1650. Lowest Common Ancestor of a Binary Tree III.java
@@ -1,4 +1,29 @@
-//My Solution: Check one by one
+//My Solution: Merge sort and find while merge, O(abs(pToRoot - qToRoot))
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Set<Node> pParents = new HashSet();
+        Set<Node> qParents = new HashSet();
+        while(p != null && q != null ){
+            pParents.add(p);
+            qParents.add(q);
+            if(pParents.contains(q)) return q;
+            if(qParents.contains(p)) return p;
+            p = p.parent;
+            q = q.parent;
+        }
+        while(p != null){
+            if(qParents.contains(p)) return p;
+            p = p.parent;
+        }
+        
+        while(q != null){
+            if(pParents.contains(q)) return q;
+            q = q.parent;
+        }
+        return null;
+    }
+}
+//My Solution: Check one by one O(q to Root + p to Root) 
 class Solution {
     public Node lowestCommonAncestor(Node p, Node q) {
         Set<Node> pParents = new HashSet();
