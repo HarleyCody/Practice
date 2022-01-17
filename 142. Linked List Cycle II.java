4971bc8525b771dac1f72488c6e184e9e08750e0
Harley <43016364+HarleyCody@users.noreply.github.com>
2022-01-17 11:31:37 -0500

    Create 142. Linked List Cycle II.java

---
+++ 142. Linked List Cycle II.java
@@ -0,0 +1,24 @@
+//My Solution: Detect the cycle first, set the slow back to head and the node that fast and slow meet is the anwser
+public class Solution{
+	public ListNode detectCycle(ListNode head){
+		ListNode slowNode = head;
+		ListNode fastNode = head;
+		while(fastNode != null && fastNode.next != null){
+	fastNode = fastNode.next.next;
+	slowNode = slowNode.next;
+	if(slowNode == fastNode){
+	break;
+}
+}
+if(fastNode == null || fastNode.next == null){
+	return null;
+}
+slowNode = head;
+while(slowNode != fastNode){
+	slowNode = slowNode.next;
+	fastNode = fastNode.next;
+}
+
+return fastNode;
+}
+}
