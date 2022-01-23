3020b041aa47340a278c9e0fb1b61e271fe6cd01
Harley <43016364+HarleyCody@users.noreply.github.com>
2022-01-22 21:06:37 -0500

    Update 2115. Find All Possible Recipes from Given Supplies.java

--- 2115. Find All Possible Recipes from Given Supplies.java
+++ 2115. Find All Possible Recipes from Given Supplies.java
@@ -26,3 +26,44 @@ class Solution{
         return supplies.add(name);
     }
 }
+
+// Topology: start from base which is ingredients and try to expand recipe to supplies
+class Solution {
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        Map<String, List<String>> adjList = new HashMap<>();
+        Map<String, Integer> indegree = new HashMap<>();
+        Set<String> recipeSet = new HashSet<>();
+        
+        for (int i = 0; i < recipes.length; i++) {
+            String recipe = recipes[i];
+            recipeSet.add(recipe);
+            List<String> ingredientOfRecipe = ingredients.get(i);
+            
+            for (String ingredient : ingredientOfRecipe) {
+                adjList.computeIfAbsent(ingredient, x -> new ArrayList<>()).add(recipe);
+                indegree.put(recipe, indegree.getOrDefault(recipe, 0) + 1);
+            }
+        }
+        
+        Deque<String> queue = new ArrayDeque<>();
+        List<String> result = new ArrayList<>();
+        
+        for (String supply : supplies) queue.add(supply);
+        
+        while (!queue.isEmpty()) {
+            String curr = queue.removeFirst();
+            if (recipeSet.contains(curr)) result.add(curr);
+            
+            if (adjList.containsKey(curr)) {
+                for (String neighbor : adjList.get(curr)) {
+                    int newOccurrence = indegree.get(neighbor) - 1;
+                    indegree.put(neighbor, newOccurrence);
+                    if (newOccurrence == 0) queue.add(neighbor);
+                }
+            }
+        }
+        
+        return result;
+        
+    }
+}
